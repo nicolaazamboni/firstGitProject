b3d74b1cde67a325d91a4583f01cfa27e5494750
nicolaazamboni <nicolazamboni19@gmail.com>
2021-11-17 11:11:57 +0100

    Added comment line 4 from rep2

--- letsgo.c
+++ letsgo.c
@@ -1,7 +1,8 @@
 /*Added comment from repository 1(machine1)*/
 #incldue <stdio.h>
 
-int main(){
+int main() //Another comment from repository 2 (machine 2)
+{
 
 printf("Hello World\n");
 printf("added line 1\n");
